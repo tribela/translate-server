917e10b15e43b6c135d571303c3f0bb872f2d63c
Jeong Arm <kjwonmail@gmail.com>
2022-11-11 14:26:59 +0900

    Support POST method

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const translator = require('./translator');
 const corsWrapper = require('./cors');
 const {send} = require('micro');
+const parse = require('urlencoded-body-parser');
 
 const cors = corsWrapper({
     allowMethods: ['GET', 'OPTIONS'],
@@ -26,7 +27,20 @@ const handler = async (req, res) => {
                 message: 'If you\'re looking to translate into japanese, you must use ja instead'
             })
         }
-        const text = splitUrl[1];
+
+        let text;
+        switch (req.method) {
+            case 'GET':
+                text = splitUrl[1];
+                break;
+            case 'POST':
+                const body = await parse(req);
+                text = body.text;
+                break;
+            default:
+                return send(res, 400, {});
+        }
+
         const decodedText = decodeURIComponent(text);
 
         const translated = await translator(decodedText, languageTo);
